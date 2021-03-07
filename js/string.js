67bd203b9b403693bffcb1a047ad41249f842055
CreativeMask <75090254+CreativeMask@users.noreply.github.com>
2021-03-06 21:30:24 -0600

    Add files via upload

---
+++ js/string.js
@@ -0,0 +1,38 @@
+class string{
+	constructor(body,anchor)
+	{
+		
+		var options={ bodyA:body,			 
+			pointB:anchor, 
+			stiffness:0.004, 
+			length:1
+			
+		}
+		
+		this.bodyA=body
+		this.pointB=anchor
+		this.string=Constraint.create(options)
+		World.add(world,this.string)
+	}
+
+	attach(body){
+		this.string.bodyA=body;
+	}
+
+	fly()
+	{
+		this.string.bodyA=null;
+	}
+
+	display()
+	{
+		if(this.string.bodyA)
+		{
+			var pointA=this.bodyA.position;
+			var pointB=this.pointB
+
+			strokeWeight(2);		
+			line(pointA.x,pointA.y,pointB.x,pointB.y);
+		}
+	}
+}
